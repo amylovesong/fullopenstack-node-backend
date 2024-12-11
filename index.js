6ff0fe7e9390501eb86a674c8b2af58a9b6f0cac
amylovesong <amylovesong.sun@gmail.com>
2024-12-11 16:45:22 +0800

    Receiving data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express') // express is a function
 const app = express()
 
+app.use(express.json()) // activate the Express json-parser
+
 let notes = [
   {
     id: "1",
@@ -45,6 +47,37 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id))) // mapping the notes to ids and finding out the largest number
+    : 0 // '...' is the 'three dot' spread syntax
+  
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  console.log('request.headers', request.headers);
+  // if the request headers' Content-Type is not 'application/json'，the request.body will be a empty JSON：'{}'
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ // code 400 means 'bad request'
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+  console.log('request.body', note);
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001
