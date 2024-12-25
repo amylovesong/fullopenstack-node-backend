4640b7add33e7c6f9ca2a801fdc257b45ee0ffa0
amylovesong <amylovesong.sun@gmail.com>
2024-12-25 15:48:20 +0800

    Run tests one by one

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -28,14 +28,14 @@ beforeEach(async () => {
   await noteObject.save()
 })
 
-test('notes are returned as json', async () => {
+test.only('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/) // the desired value is defined as regular expression or in short regex
 })
 
-test('there are two notes', async () => {
+test.only('there are two notes', async () => {
   const response = await api.get('/api/notes')
 
   assert.strictEqual(response.body.length, initialNotes.length)
